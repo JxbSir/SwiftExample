5d2315a81f5cebc47025a1bf8def0f71bf93a09e
PeterJin <i@jxb.name>
2016-05-10 14:24:26 +0800

    移除pod

--- Pods/Target Support Files/JxbPhotoView_Swift/JxbPhotoView_Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JxbPhotoView_Swift : NSObject
-@end
-@implementation PodsDummy_JxbPhotoView_Swift
-@end
--- Pods/Target Support Files/JxbPhotoView_Swift/JxbPhotoView_Swift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double JxbPhotoView_SwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char JxbPhotoView_SwiftVersionString[];
-
